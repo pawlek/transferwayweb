52d2fc4894a9758b1bda97a130e96472a744573c
pawlek <pavlo.voronyuk@gmail.com>
2020-12-17 20:53:17 +0100

    upd

--- src/components/action.php
+++ src/components/action.php
@@ -7,23 +7,24 @@
     
     try {
         $connect = new PDO("mysql:host=$DBHOST; dbname=$DBNAME;", $DBUSER, $DBPASS);
+        $received_data = json_decode(file_get_contents("php://input"));
+        $data = array();
+        if($received_data->action == 'fetchall'){
+            $query = "
+                SELECT * FROM rides
+            ";
+            $statement = $connect->prepare($query);
+            $statement->execute();
+            while($row = $statement->fetch(PDO::FETCH_ASSOC)){
+                $data[] = $row;
+            }
+            echo json_encode($data);
+        }
     } catch (PDOException $e) {
         echo 'Подключение не удалось: ' . $e->getMessage();
     }
 
-    $received_data = json_decode(file_get_contents("php://input"));
-    $data = array();
-    if($received_data->action == 'fetchall'){
-        $query = "
-            SELECT * FROM rides
-        ";
-        $statement = $connect->prepare($query);
-        $statement->execute();
-        while($row = $statement->fetch(PDO::FETCH_ASSOC)){
-            $data[] = $row;
-        }
-        echo json_encode($data);
-    }
+    
 
     // $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 	// $connection->set_charset("utf8");
