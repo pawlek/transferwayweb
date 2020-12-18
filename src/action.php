b0d683b91f0b1627fea27a85734918f5448b7339
pawlek <pavlo.voronyuk@gmail.com>
2020-12-18 12:29:20 +0100

    upd

--- src/action.php
+++ src/action.php
@@ -1,19 +1,24 @@
 <?php
-    $host = 'extraver.mysql.tools';
-    $user = 'extraver_taxiairport';
-    $password = '-vV-yU6z57';
-    $dbname = 'extraver_taxiairport';
+    // $host = 'extraver.mysql.tools';
+    // $user = 'extraver_taxiairport';
+    // $password = '-vV-yU6z57';
+    // $dbname = 'extraver_taxiairport';
+
+    $host = '212.44.113.12';
+    $user = 'intaxi16_transferway';
+    $password = '*OMCqHk{[%kH';
+    $dbname = 'intaxi16_transferway';
 
     $connect = new PDO("mysql:host=$host;dbname=$dbname", "$user", "$password");
     $received_data = json_decode(file_get_contents("php://input"));
     $data = array();
     if($received_data->action == 'fetchall'){
         $query = "
-        SELECT * FROM rides 
+        SELECT * FROM orders 
         ";
         $statement = $connect->prepare($query);
         $statement->execute();
-        
+
         while($row = $statement->fetch(PDO::FETCH_ASSOC)){
             $data[] = $row;
         }
